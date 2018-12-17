b6c2bc06dd1491165a2a1b62ec54405f3ae35b27
zqc <1023920138@qq.com>
2018-12-17 16:51:18 +0800

    工商

--- src/main/java/jll/reportedData/service/BusinessInformationService.java
+++ src/main/java/jll/reportedData/service/BusinessInformationService.java
@@ -1,13 +1,8 @@
 package jll.reportedData.service;
 
-import jll.model.BasicPnformationSubmit.CoreEntering;
 
 public interface BusinessInformationService {
 
-    /**
-     * 添加工商信息
-     * */
-    public void addBusinessInformation(CoreEntering coreEntering);
 
 
 }
